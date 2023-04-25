8e5508134abb24c903b6d382f44507c87b85579a
Kalen <kalenwillits@gmail.com>
2023-04-25 00:57:57 -0400

    schema imports fix

--- src/hub/core/schemas/__init__.py
+++ src/hub/core/schemas/__init__.py
@@ -4,6 +4,13 @@ from .project import PushProjectIn
 from .project import SetProjectIn
 from .project import DropProjectIn
 from .project import PullProjectOut
+
+from .sprint import UseSprintIn
+from .sprint import NewSprintIn
+from .sprint import PushSprintIn
+from .sprint import SetSprintIn
+from .sprint import DropSprintIn
+
 from .sprint import PullSprintOut
 from .task import PullTaskOut
 from .comment import PullCommentOut
