19520eac3bf4e738d47f7e121cf6c0ba9a525c32
Kalen <kalenwillits@gmail.com>
2023-04-26 17:31:35 -0400

    Swapping priorities to use Python for cli instead for better REST support.

--- doc/todolist.md
+++ doc/todolist.md
@@ -1,2 +1,7 @@
 - unit test all api calls 
-- log objects on all api calls.
+
+
+Switch to curlpp
+https://github.com/jpbarrette/curlpp/blob/master/examples/example00.cpp
+
+
--- run/build.sh
+++ run/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-export CPLUS_INCLUDE_PATH=\
-$PWD/src:\
-$PWD/src/cli/include/HTTPRequest:\
-$PWD/src/cli/include/HTTPRequest/include;
-
 g++ -std=c++20 $PWD/src/cli/*.cpp -o $PWD/bin/mana.64;
 
